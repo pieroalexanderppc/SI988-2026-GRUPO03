9b009aa0ca26bb47f05b8d21e5e220f01ec6006c
pieroalexanderppc <pp2020067576@virtual.upt.pe>
2026-09-17 09:23:28 -0500

    feat(firebase): configurar e inicializar Firebase (#7)

--- proyecto_moviles2_grupo03/lib/main.dart
+++ proyecto_moviles2_grupo03/lib/main.dart
@@ -3,7 +3,14 @@ import 'package:provider/provider.dart';
 import 'providers/componentes_provider.dart';
 import 'screens/home_screen.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const PromedioApp());
 }
 
